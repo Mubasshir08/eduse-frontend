2a5221913cf7dbe4822a732773d6ecd9df8b8a67
Nafees_Ahmed <88581633+nafeesahmed11@users.noreply.github.com>
2025-09-01 18:39:19 +0600

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Blog from "./pages/Blog"
+import Navbar from "./Nav"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Blog />   
+    <Navbar/>   
     </>
   )
 }
