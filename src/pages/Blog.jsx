e37ba2186b4bb83e4b5d50ed7574a67147968f51
Nafees_Ahmed <88581633+nafeesahmed11@users.noreply.github.com>
2025-09-03 18:32:00 +0600

    page complete

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,11 @@ import Cardvido from "../assets/video/v1.mp4"
 import Cardvidtwo from "../assets/video/v2.mp4"
 import Cardvidthre from "../assets/video/v3.mp4"
 import Cardvidfo from "../assets/video/v4.mp4"
+import Cardvidfit from "../assets/video/v5.mp4"
+import Cardvidsix from "../assets/video/v6.mp4"
+import Cardvidseven from "../assets/video/v7.mp4"
+import Cardvieight from "../assets/video/v8.mp4"
+import Cardvidnine from "../assets/video/v9.mp4"
 
 
 
@@ -22,9 +27,7 @@ const Blog = () => {
         <div class="card bg-base-100 w-96 shadow-md  rounded-b-md">
         <figure>
           {/* video */}
-          <video  controls src={Cardvido} type="video/mp4" className='rounded-t-md'/>
-    
-          
+          <video  controls src={Cardvido} type="video/mp4" className='rounded-t-md'/>  
       </figure>
      <div className='p-2'>
        <div div class="card-body">
@@ -115,6 +118,132 @@ Eduse’s certified courses......
 </div>
 
     {/* 4th card end */}
+
+    {/* 5th card start */}
+    
+    
+          <div class="card bg-base-100 w-96 shadow-md rounded-b-md ">
+        <figure>
+          {/* video */}
+          <video  controls src={Cardvidfit} type="video/mp4" className='rounded-t-md'/>
+      </figure>
+     <div className='p-2'>
+       <div div class="card-body">
+        <h2 class=" font-bold text-xl">What Makes Eduse Different from Other Online Learning Platforms?</h2>
+         <a href="#">
+    <p className=' text-blue-500 hover:text-blue-600  '>why-choose-eduse-unique-benefits</p>
+    </a>
+        <p className='mb-2'>Eduse isn't just another e-learning site—it’s a student-centered community. Here’s why learners and sellers are switching to Eduse.......  
+        <a href="#">
+    <span className='underline underline-offset-1 text-blue-500 hover:text-blue-600  '> Read More</span>
+    </a>
+    </p>
+  </div>
+     </div>
+</div>
+
+    {/* 5th card end */}
+
+     {/* 6th card start */}
+    
+    
+          <div class="card bg-base-100 w-96 shadow-md rounded-b-md ">
+        <figure>
+          {/* video */}
+          <video  controls src={Cardvidsix} type="video/mp4" className='rounded-t-md'/>
+      </figure>
+     <div className='p-2'>
+       <div div class="card-body">
+        <h2 class=" font-bold text-xl">From Campus to Career – How Eduse Bridges the Gap</h2>
+         <a href="#">
+    <p className=' text-blue-500 hover:text-blue-600  '>campus-to-career-with-eduse</p>
+    </a>
+        <p className='mb-2'>Are you graduating soon but feel unprepared for the job market? Eduse equips you with real-world skills that companies actually care about.......  
+        <a href="#">
+        <span className='underline underline-offset-1 text-blue-500 hover:text-blue-600  '> Read More</span>
+    </a>
+    </p>
+  </div>
+     </div>
+</div>
+
+    {/* 6th card end */}
+      {/* 7th card start */}
+    
+    
+          <div class="card bg-base-100 w-96 shadow-md rounded-b-md ">
+        <figure>
+          {/* video */}
+          <video  controls src={Cardvidseven} type="video/mp4" className='rounded-t-md'/>
+      </figure>
+     <div className='p-2'>
+       <div div class="card-body">
+        <h2 class=" font-bold text-xl">10 Creative Product Ideas You Can Sell as a Student</h2>
+         <a href="#">
+    <p className=' text-blue-500 hover:text-blue-600  '>student-product-ideas-to-sell-online</p>
+    </a>
+        <p className='mb-2'>Want to start earning but don’t know what to sell? Here are 10 simple yet powerful product ideas perfect for students to sell on Eduse Marketplace......
+ <a href="#">
+        <span className='underline underline-offset-1 text-blue-500 hover:text-blue-600  '> Read More</span>
+    </a>
+    </p>
+  </div>
+     </div>
+</div>
+
+    {/* 7th card end */}
+
+     {/* 8th card start */}
+    
+    
+          <div class="card bg-base-100 w-96 shadow-md rounded-b-md ">
+        <figure>
+          {/* video */}
+          <video  controls src={Cardvieight} type="video/mp4" className='rounded-t-md'/>
+      </figure>
+     <div className='p-2'>
+       <div div class="card-body">
+        <h2 class=" font-bold text-xl">Learn at Your Pace, In Your Language The Eduse Advantage</h2>
+         <a href="#">
+    <p className=' text-blue-500 hover:text-blue-600  '>learn-in-your-language-eduse-benefits</p>
+    </a>
+        <p className='mb-2'>Language shouldn’t be a barrier to learning. Eduse offers multilingual content to ensure that every student feels confident and included....... 
+ <a href="#">
+        <span className='underline underline-offset-1 text-blue-500 hover:text-blue-600  '> Read More</span>
+    </a>
+    </p>
+  </div>
+     </div>
+</div>
+
+    {/* 8th card end */}
+
+     {/* 9th card start */}
+    
+    
+          <div class="card bg-base-100 w-96 shadow-md rounded-b-md ">
+        <figure>
+          {/* video */}
+          <video  controls src={Cardvidnine} type="video/mp4" className='rounded-t-md'/>
+      </figure>
+     <div className='p-2'>
+       <div div class="card-body">
+        <h2 class=" font-bold text-xl">Microlearning – Small Steps, Big Results</h2>
+         <a href="#">
+    <p className=' text-blue-500 hover:text-blue-600  '>microlearning-eduse-benefits</p>
+    </a>
+        <p className='mb-2'>Busy schedule? No problem. With Eduse’s microlearning 
+format, you can master a new skill in just 10 minutes
+a day.......
+ <a href="#">
+        <span className='underline underline-offset-1 text-blue-500 hover:text-blue-600  '> Read More</span>
+    </a>
+    </p>
+  </div>
+     </div>
+</div>
+
+    {/* 9th card end */}
 
       
       </div>
