9f5b314fb90e6b40238218a0117766852627e9bf
Nafees_Ahmed <88581633+nafeesahmed11@users.noreply.github.com>
2025-09-06 14:30:25 +0600

    footer half

---
+++ src/shraed/Footer.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Llogo from "../assets/images/llogo.png"
+
+const Footer = () => {
+  return (
+    <footer className='px-30 xl:px-40 md:px-90   my-20 '>
+      {/* top */}
+     <section className='flex justify-between  '>
+       {/* part-1 */}
+      <div className=' flex flex-col '>
+       <div>
+         <img src={Llogo} alt="img" />
+         </div>
+        <div className='w-[274px] text-center py-5 '>
+          <p className='text-[32px]  text-[#666666]'>A online Platform</p>
+        </div>
+      </div>
+      {/* part-2 */}
+      <div className='flex justify-between mt-12 w-[70%]'>
+        {/* ch-1 */}
+        <div>
+            <h className=' font-bold text-[24px]'>About us</h>
+
+          <ul className='list-disc p-6'>
+            <li className='text-[#666666] hover:text-black'><a href="#">Careers</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Press & News</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Partnerships</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Privacy Policy</a></li> 
+            <li className='text-[#666666] hover:text-black'><a href="#">Terms of Service</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Intellectual Property Claims</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Investor Relations</a></li>
+          </ul>
+        </div>
+        {/* ch-2 */}
+         <div>
+            <h className=' font-bold text-[24px]'>Support and Education</h>
+
+          <ul className='list-disc p-6'>
+            <li className='text-[#666666] hover:text-black'><a href="#">Help & Support</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Trust & Safety</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Selling on eduse</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Buying on eduse</a></li> 
+            <li className='text-[#666666] hover:text-black'><a href="#">Eduse Guides</a></li>
+            
+          </ul>
+        </div>
+       {/* ch-3 */}
+         <div>
+            <h className=' font-bold text-[24px]'>Community</h>
+
+          <ul className='list-disc p-6'>
+            <li className='text-[#666666] hover:text-black'><a href="#">Customer Success Stories</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Community Hub</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Forum</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">
+Events</a></li> 
+            <li className='text-[#666666] hover:text-black'><a href="#">Blog</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Creators</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Invite a Friend</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Become a Seller</a></li>
+            <li className='text-[#666666] hover:text-black'><a href="#">Community Standards</a></li>
+            
+          </ul>
+        </div>
+      </div>
+     </section>
+     {/* bottom */}
+  
+    </footer>
+  )
+}
+
+export default Footer
