0f686a2e865b75b7c6c97fc9ee87517afdcc34e2
Mubasshir08 <msr4212019@gmail.com>
2025-10-20 11:19:39 +0600

    add footer on checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Checkout from "./Pages/Checkout";
 import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
 
 const App = () => {
   return (
     <>    
       <Navbar />
       <Checkout />
+      <Footer />
     </>
   );
 };
