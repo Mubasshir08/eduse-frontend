7fa5cde00a500df2c34745125f4b376e35ea3243
Mubasshir08 <msr4212019@gmail.com>
2025-10-26 11:25:22 +0600

    profile page done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='p-3 text-[#666666]'>
+      <h3 className='text-2xl font-semibold tracking-wide mb-3'>My Profile</h3>
+      <h5 className='pb-2'>First Name : </h5>
+      <h5 className='pb-2'>Last Name :</h5>
+      <h5 className='pb-2'>Username :</h5>
+      <h5 className='pb-2'>Email :</h5>
+    </div>
   )
 }
 
