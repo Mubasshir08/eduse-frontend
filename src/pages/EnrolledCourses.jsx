962430be7becf9478417d1312752bdff4fbcb809
Mubasshir08 <msr4212019@gmail.com>
2025-10-26 11:51:52 +0600

    enrolled courses page done

--- src/pages/EnrolledCourses.jsx
+++ src/pages/EnrolledCourses.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EnrolledCourses = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [isEnrolledCourses, setIsEnrolledCourses] = useState(true);
+  const [isActiveCourses, setIsActiveCourses] = useState(false);
+  const [isCompleteCourses, setIsCompleteCourses] = useState(false);
   return (
-    <div>EnrolledCourses</div>
+    <div className='text-[#666666]'>
+      <h3 className='text-lg font-semibold tracking-wide mb-3'>
+        Enrolled Courses
+      </h3>
+      <div className='flex gap-x-3 border-b-[1px] pb-3'>
+        <button className= {`hover:text-[#015AD8] ${activeTab === 0 ? 'text-[#015AD8]' : ''}`} onClick={() => setActiveTab(0) && setIsEnrolledCourses(true)}>
+          Enrolled Courses(1)
+        </button>
+        <button className= {`hover:text-[#015AD8] ${activeTab === 1 ? 'text-[#015AD8]' : ''}`} onClick={() => setActiveTab(1) && setIsActiveCourses(true)}>Active Courses(1)</button>
+        <button className= {`hover:text-[#015AD8] ${activeTab === 2 ? 'text-[#015AD8]' : ''}`} onClick={() => setActiveTab(2) && setIsCompleteCourses(true)}>Complete Courses</button>
+      </div>
+    </div>
   )
 }
 
