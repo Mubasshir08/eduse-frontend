91c3726a3f5bd85fdb6fb01c31577b4feaceb00c
Mubasshir08 <msr4212019@gmail.com>
2025-10-28 16:47:06 +0600

    empowering-students-combine page fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from "react";
+import Empowering_Student_Combine from "./Pages/Empowering_Student_Combine";
 
-import Navbar from "./Components/Pages/Navbar";
-import Hero from "./Components/Pages/Hero";
-import Courses from "./Components/Pages/PopularCourses";
-import Categories from "./Components/Pages/Categories";
-import Testimonials from "./Components/Pages/Testimonials";
-import Blogs from "./Components/Pages/Blogs";
-import FAQ from "./Components/Pages/FAQ";
-import Journey from "./Components/Pages/CTA";
-import Footer from "./Components/Pages/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Courses />
-      <Categories />
-      <Testimonials />
-      <Blogs />
-      <FAQ />
-      <Journey />
-      <Footer />
+      <Empowering_Student_Combine />
     </div>
   );
 }
