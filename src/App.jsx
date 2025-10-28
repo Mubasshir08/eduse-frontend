87312a5edb1af5df0b4672474034f57a63885768
Mubasshir08 <msr4212019@gmail.com>
2025-10-28 14:39:59 +0600

    move shared folder from components to src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Checkout from "./pages/Checkout";
-import Navbar from "./components/shared/Navbar";
-import Footer from "./components/shared/Footer";
+import Navbar from "./shared/Navbar";
+import Footer from "./shared/Footer";
 
 const App = () => {
   return (
