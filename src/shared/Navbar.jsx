249c46865a10c978a03efd2099e385cf77ab760e
Mubasshir08 <msr4212019@gmail.com>
2025-10-28 17:33:01 +0600

    fix some path of logo of footer and navbar

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import Logo from "../../assets/images/eduse-logo.png"
+import Logo from "../assets/images/eduse-logo.png"
 import { PiShoppingCartLight } from "react-icons/pi";
 
 const Navbar = () => {
