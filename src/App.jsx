84125b0c784a4d9bba6fe79c8d61065d9296d09a
Mubasshir08 <msr4212019@gmail.com>
2025-10-30 14:13:10 +0600

    merge course details branch into main branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./pages/SignUp";
 import Id_Recovery from "./pages/Id_Recovery";
 import SignIn from "./pages/SignIn";
 import Blog from "./pages/Blog"
+import CoursePage from "./pages/CoursePage";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <SignUp />
       <Id_Recovery />
       <Blog/>
+      <CoursePage />
     </>
   );
 };
