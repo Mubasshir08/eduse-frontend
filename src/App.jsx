4b75429d5815d34ecf8e88e4975dd29349ee1681
Mubasshir08 <msr4212019@gmail.com>
2025-10-30 13:41:54 +0600

    Save current changes before merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignUp from "./Pages/SignUp";
 import Id_Recovery from "./Pages/Id_Recovery";
+import SignIn from "./Pages/SignIn";
 
 const App = () => {
   return (
-    <>  
+    <> 
+      <SignIn /> 
       <SignUp />
       <Id_Recovery />
     </>
